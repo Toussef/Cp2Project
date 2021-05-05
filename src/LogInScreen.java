7f37e90258870fb2a7491c99b6a73eaaa8471b66
Toussef <youssef@mmneimneh.net>
2021-05-05 22:36:13 +0300

    Initial commit

---
+++ src/LogInScreen.java
@@ -0,0 +1,107 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JToolBar;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
+import javax.swing.JPasswordField;
+
+public class LogInScreen extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField emailInput;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LogInScreen frame = new LogInScreen();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LogInScreen() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 563, 372);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JToolBar toolBar = new JToolBar();
+		toolBar.setBounds(0, 0, 120, 30);
+		contentPane.add(toolBar);
+		
+		JButton helpButton = new JButton("Help");//help button found within the toolbar.
+	
+		helpButton.addMouseListener(new MouseAdapter() {
+			JTextField helpText = new JTextField();//line containing help tool tip.
+			@Override
+			public void mouseEntered(MouseEvent e) {
+		//	JTextField helpText = new JTextField();//line containing help tool tip.
+			helpText.setText("Please contact us over at CYHtravel.com");
+			helpText.setBounds(10,11,158,40);
+			helpText.setFont(helpText.getFont().deriveFont((9)));
+			contentPane.add(helpText);
+			}
+			@SuppressWarnings("deprecation")
+			public void mouseExited(MouseEvent e) {
+			helpText.hide();
+			}
+			
+		});
+		toolBar.add(helpButton);
+		
+		emailInput = new JTextField();
+		emailInput.setHorizontalAlignment(SwingConstants.CENTER);
+		emailInput.setBounds(176, 109, 218, 22);
+		contentPane.add(emailInput);
+		emailInput.setColumns(10);
+		
+		JTextArea emailBox = new JTextArea();
+		emailBox.setEditable(false);
+		emailBox.setText("Email");
+		emailBox.setBounds(176, 76, 45, 22);
+		contentPane.add(emailBox);
+		
+		JTextArea passBox = new JTextArea();
+		passBox.setText("Password");
+		passBox.setEditable(false);
+		passBox.setBounds(176, 142, 84, 22);
+		contentPane.add(passBox);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(176, 175, 218, 22);
+		contentPane.add(passwordField);
+		
+		JButton btnNewButton = new JButton("Log-in!");
+		btnNewButton.setBounds(229, 237, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JTextArea txtrV = new JTextArea();
+		txtrV.setText("V 0.1");
+		txtrV.setEditable(false);
+		txtrV.setBounds(495, 313, 54, 22);
+		contentPane.add(txtrV);
+	}
+}
---
+++ src/Login_Register.java
@@ -0,0 +1,104 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+import java.awt.BorderLayout;
+import javax.swing.JToolBar;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Login_Register {
+
+	private JFrame frame;
+	private JTextField txtV;
+	private JTextField txtCyhTravel;
+	private JButton btnNewButton;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login_Register window = new Login_Register();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Login_Register() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 555, 427);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JToolBar toolBar = new JToolBar();
+		toolBar.setBounds(0, 0, 144, 20);
+		frame.getContentPane().add(toolBar);
+		
+		JButton helpButton = new JButton("Help");//help button found within the toolbar.
+		helpButton.addMouseListener(new MouseAdapter() {
+			JTextField helpText = new JTextField();//line containing help tool tip.
+			@Override
+			public void mouseEntered(MouseEvent e) {
+		//	JTextField helpText = new JTextField();//line containing help tool tip.
+			helpText.setText("Please contact us over at CYHtravel.com");
+			helpText.setBounds(10,11,158,30);
+			helpText.setFont(helpText.getFont().deriveFont((9)));
+			frame.getContentPane().add(helpText);
+			}
+			@SuppressWarnings("deprecation")
+			public void mouseExited(MouseEvent e) {
+			helpText.hide();
+			}
+			
+		});
+		toolBar.add(helpButton);
+		
+		txtV = new JTextField();
+		txtV.setText("V0.1");
+		txtV.setBounds(509, 370, 32, 20);
+		frame.getContentPane().add(txtV);
+		txtV.setColumns(10);
+		
+		txtCyhTravel = new JTextField();
+		txtCyhTravel.setEditable(false);
+		txtCyhTravel.setHorizontalAlignment(SwingConstants.CENTER);
+		txtCyhTravel.setText("CYH Travel");
+		txtCyhTravel.setBounds(195, 47, 124, 70);
+		frame.getContentPane().add(txtCyhTravel);
+		txtCyhTravel.setColumns(10);
+		
+		Icon logIn = new ImageIcon("loginRS.png");
+		JButton loginButton = new JButton(logIn);
+		loginButton.setBounds(96, 185, 100, 51);
+		frame.getContentPane().add(loginButton);
+		
+		Icon registerPic = new ImageIcon("registerRS.jpg");
+		JButton register = new JButton(registerPic);
+		register.setBounds(338, 185, 100, 51);		
+		frame.getContentPane().add(register);
+	}
+}
