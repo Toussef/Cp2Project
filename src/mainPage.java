8913afaf26941128659733c51abe530e5d08eb09
Toussef <youssef@mmneimneh.net>
2021-05-05 23:17:35 +0300

    Create mainPage.java

---
+++ src/mainPage.java
@@ -0,0 +1,46 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextArea;
+
+public class mainPage extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					mainPage frame = new mainPage();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public mainPage() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 543, 362);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JTextArea txtrV = new JTextArea();
+		txtrV.setText("V 0.1");
+		txtrV.setBounds(468, 303, 61, 22);
+		contentPane.add(txtrV);
+	}
+
+}
